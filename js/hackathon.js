9e625c4844bf71fd4c146173ee5013d6cdeaa875
Ganesh8903 <ganesh.nexitance@gmail.com>
2024-02-25 19:08:09 +0530

    hackthon

--- js/hackathon.js
+++ js/hackathon.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const registerButton = document.getElementById("registerButton");
   if (registerButton) {
-    registerButton.addEventListener("click", function () { 
-      window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeWxHu_aL7J_CAGwXNn3s5YOO1M0DAmjml87xDj3pF8iA-fcg/viewform";
+    registerButton.addEventListener("click", function () {
+      window.location.href =
+        "https://docs.google.com/forms/d/e/1FAIpQLSeWxHu_aL7J_CAGwXNn3s5YOO1M0DAmjml87xDj3pF8iA-fcg/viewform";
     });
   }
   document.body.classList.add("loaded");
 });
-
 
 function showDetails(stepNumber) {
   let detailsContainer = document.getElementById("details-container");
@@ -28,14 +28,14 @@ function showDetails(stepNumber) {
       detailsContainer.innerHTML = `
       <div class='step-with-image'>
         <img src='images/Teamsmall_image.jpg' alt='Team Image' class='background-image' />
-        <p class='step-text' style='color: #000;font-size: 40px;'>${details}</p>
+        <p class='step-text' style='color:#FFFF;text-shadow:0 0 10px #fc6c85;font-size: 40px;'>${details}</p>
       </div>`;
       break;
     case 3:
       details = "Workshop and Mentoring Sessions at 8:00 pm.";
       detailsContainer.innerHTML = `
       <div class='step-with-image'>
-        <img src='images/Worksmall_image.jpg' alt='Workshop Image' class='background-image' />
+        <img src='images/teamwork_image.jpg' alt='Workshop Image' class='background-image' />
         <p class='step-text' style='color: #000;font-size: 40px;'>${details}</p>
       </div>`;
       break;
@@ -43,7 +43,7 @@ function showDetails(stepNumber) {
       details = "Project Submission starts at 10:00 am.";
       detailsContainer.innerHTML = `
       <div class='step-with-image'>
-        <img src='images/Worksmall_image.jpg' alt='Workshop Image' class='background-image' />
+        <img src='images/project_image.png' alt='Workshop Image' class='background-image' />
         <p class='step-text' style='color: #000;font-size: 40px;'>${details}</p>
       </div>`;
       break;
@@ -52,7 +52,7 @@ function showDetails(stepNumber) {
         "Judging and Evaluation begin at 11:00 am followed by the Awards at 12:00 pm.";
       detailsContainer.innerHTML = `
       <div class='step-with-image'>
-        <img src='images/Worksmall_image.jpg' alt='Workshop Image' class='background-image' />
+        <img src='images/awards_image.jpg' alt='Workshop Image' class='background-image' />
         <p class='step-text' style='color: #000;font-size: 40px;'>${details}</p>
       </div>`;
       break;
@@ -65,7 +65,6 @@ function showDetails(stepNumber) {
   document.querySelectorAll(".step").forEach((step) => {
     step.classList.remove("active");
   });
-
 
   const clickedStep = document.querySelector(
     `.step[data-step="${stepNumber}"]`
